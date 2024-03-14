d223593ef7d5726c2ce18df97e7de31d61699c84
Paul Miller <paul@paulmillr.com>
2024-03-14 08:34:13 +0000

    Add build step for standalone file

---
+++ build/input.js
@@ -0,0 +1,9 @@
+export { Transaction } from '../lib/esm/index.js';
+export {
+  p2pk, p2pkh, p2sh, p2ms, p2wsh, p2wpkh, p2tr, p2tr_ns, p2tr_ms, p2tr_pk,
+} from '../lib/esm/index.js';
+export { OP, RawTx, CompactSize, Script, ScriptNum } from '../lib/esm/index.js';
+export { selectUTXO } from '../lib/esm/index.js';
+export { NETWORK, TAPROOT_UNSPENDABLE_KEY } from '../lib/esm/index.js';
+export { Address, getAddress, WIF, OutScript } from '../lib/esm/index.js';
+export { Decimal, bip32Path, SigHash, PSBTCombine } from '../lib/esm/index.js';
