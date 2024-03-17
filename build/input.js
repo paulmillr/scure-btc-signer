6604c65460a6ee3c57cc0bf46d4dea0696794ca7
Paul Miller <paul@paulmillr.com>
2024-03-17 12:16:41 +0000

    Use jsbt for tsconfig and building

--- build/input.js
+++ build/input.js
@@ -1,9 +1,9 @@
-export { Transaction } from '../lib/esm/index.js';
+export { Transaction } from 'lib';
 export {
   p2pk, p2pkh, p2sh, p2ms, p2wsh, p2wpkh, p2tr, p2tr_ns, p2tr_ms, p2tr_pk,
-} from '../lib/esm/index.js';
-export { OP, RawTx, CompactSize, Script, ScriptNum } from '../lib/esm/index.js';
-export { selectUTXO } from '../lib/esm/index.js';
-export { NETWORK, TAPROOT_UNSPENDABLE_KEY } from '../lib/esm/index.js';
-export { Address, getAddress, WIF, OutScript } from '../lib/esm/index.js';
-export { Decimal, bip32Path, SigHash, PSBTCombine } from '../lib/esm/index.js';
+} from 'lib';
+export { OP, RawTx, CompactSize, Script, ScriptNum } from 'lib';
+export { selectUTXO } from 'lib';
+export { NETWORK, TAPROOT_UNSPENDABLE_KEY } from 'lib';
+export { Address, getAddress, WIF, OutScript } from 'lib';
+export { Decimal, bip32Path, SigHash, PSBTCombine } from 'lib';
